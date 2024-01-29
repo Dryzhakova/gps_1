ff890e0d81d0d86b1ad54705a9a1265b31fd324a
Katry0_0 <dryzhakova.kt@gmail.com>
2024-01-29 01:42:21 +0100

    Initial commit

---
+++ app/src/main/java/com/example/gps_1/Fragments/EmergContactFragment.kt
@@ -0,0 +1,23 @@
+package com.example.gps_1.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.gps_1.R
+
+class EmergContactFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_emerg_contact, container, false)
+    }
+
+
+}
